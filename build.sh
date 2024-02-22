befa70b594d468f038b78bc4d480026b2721fb6a
FlamesRunner <akyle2001@gmail.com>
2024-02-21 23:57:05 -0500

    Move to GitHub action

--- build.sh
+++ build.sh
@@ -9,17 +9,11 @@ for VAR in ${ENV_VARS[@]}; do
   fi
 done
 
-# Check if root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
-fi
-
 echo "Generating thumbnails and WEBP images for the gallery..."
 
 # Download mogrify and awscli
-apt-get update -y &> /dev/null
-apt-get install awscli imagemagick -y &> /dev/null
+sudo apt-get update -y &> /dev/null
+sudo apt-get install awscli imagemagick -y &> /dev/null
 
 # Enter the gallery directory
 cd ./src/assets/media/gallery
