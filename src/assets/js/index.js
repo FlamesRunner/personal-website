37d9c451a83701dea3bf6697ded10b37e14e82ea
FlamesRunner <akyle2001@gmail.com>
2024-02-24 21:39:19 -0500

    Force video playback on mobile

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -203,7 +203,16 @@
         year.innerHTML = new Date().getFullYear();
 
         // Start interval
-        window.requestAnimationFrame(trace);
+        // window.requestAnimationFrame(trace);
+
+        // Add window click event listener
+        window.addEventListener("click", (event) => {
+            // If the video with id rt-video is not playing, play it
+            const video = document.getElementById("rt-video");
+            if (video.paused) {
+                video.play();
+            }
+        });
     }
 
     window.onresize = function () {
